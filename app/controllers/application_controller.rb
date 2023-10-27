670876b3699836de1814ff1e05ad9140e0bfdd7b
Kris Engel <kris.engel.ca@gmail.com>
2023-10-27 09:47:31 -0500

    changed spacing, hid elements when not populated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :link_spells
+
+  def link_spells(text)
+    return "" if text.nil?
+    Spell.all.each do |spell|
+      text.gsub!(spell.spell, "<a href='/spells/#{spell.id}'>#{spell.spell}</a>")
+    end
+    text.html_safe
+  end
 end
